0aa286ad09397223633866d4d897b5ba5d5cbdfa
DearFridayZ <121447547+DearFridayZ@users.noreply.github.com>
2023-01-30 14:29:13 +0400

    Add files via upload

---
+++ c++ learning/first.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+int main(){
+    //ლექცია 23 ...................................
+    //დავალება ნ.1
+
+    char letter = 'T';
+    int Number = 100;
+
+    //დავალება ნ.2
+
+    float num1 = 10.10;
+    double num2 = 50.23;
+
+    //დავალება ნ.3
+
+    int a = 5, b = 10, c = 15, d = 20, e = 30;
+    int sum = a + b + c + d + e;
+    float average = sum / 5;
+
+    std::cout << sum << '\n';
+
+    std::cout << average << '\n';
+
+    return 0;
+}
